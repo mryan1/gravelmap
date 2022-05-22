f163a6cd0d1ef21c33af618a8f57106df9ac6e59
Glitch (inconclusive-caring-digit) <none>
2022-05-22 03:43:41 +0000

    #️⃣👾 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
 
@@ -15,6 +17,17 @@ app.use(express.static("public"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
+
+//need endpoint to trigger import from sources
+//should import gpx, convert to one geojson and add to dir
+
+//endpoint for getting all geojson trails in one json object
+app.get("/trails", (request, response) => {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "./geojson/tails.json"));
+  let trails = JSON.parse(rawdata);
+  response.send(trails)
+});
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
